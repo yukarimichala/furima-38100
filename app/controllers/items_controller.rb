593310d0db5c184d7bd30f6f09a1b86b05b7da8d
yukarimichala <tyukari0124@gmail.com>
2022-09-17 22:52:29 +0900

    エラーの修正を検討

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
 
-  def index 
-    @items = Item.includes(:user)
+  def index
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
-  def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
-  end
+  
 
   def new
     @item = Item.new
@@ -22,6 +21,16 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 
 end
