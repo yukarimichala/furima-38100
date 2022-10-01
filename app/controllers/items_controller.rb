f5f8623d8a3fcebc589174792cfb404d38112cd4
yukarimichala <tyukari0124@gmail.com>
2022-10-01 18:49:14 +0900

    editのコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,10 +24,10 @@ class ItemsController < ApplicationController
 
   def edit
     # ログインしているユーザーと同一であればeditファイルが読み込まれる
-    if @item.user_id == current_user.id && @item.order.nil?
-    else
-      redirect_to root_path
-   end
+    #if @item.user_id == current_user.id && @item.order.nil?
+    #else
+     # redirect_to root_path
+   #end
   end
 
   def show
