cda0bc0d579ff663167f42ef089faebd5c423547
yukarimichala <tyukari0124@gmail.com>
2022-10-01 18:37:16 +0900

    ログアウト状態での商品詳細修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item , only: [:show]
 
 
@@ -22,13 +22,13 @@ class ItemsController < ApplicationController
     end
   end
 
-  #def edit
+  def edit
     # ログインしているユーザーと同一であればeditファイルが読み込まれる
-    #if @item.user_id == current_user.id && @item.order.nil?
-    #else
-    #  redirect_to root_path
-    #end
-  #end
+    if @item.user_id == current_user.id && @item.order.nil?
+    else
+      redirect_to root_path
+   end
+  end
 
   def show
   end
