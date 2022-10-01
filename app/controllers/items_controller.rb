478e539f10c61406f8c750aee1f0f8562b7f49e2
yukarimichala <tyukari0124@gmail.com>
2022-10-01 15:34:50 +0900

    商品詳細viewコントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  #before_action :set_item , only: [:show, :edit, :update, :destroy]
+  before_action :set_item , only: [:show, :edit, :update, :destroy]
 
 
   def index
@@ -36,8 +36,8 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 
-  #def set_item
-    #@item = Item.find(params[:id])
-  #end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
 end
